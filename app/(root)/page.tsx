f12a1224e1d99a70939bd7063421492f6acce07f
Sahil Kotian <sahil.kotian316@gmail.com>
2024-12-08 13:44:53 -0500

    Develop Header For Middle Section Layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,30 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <div>HOME</div>;
+  const loggedIn = { firstName: "Sahil" };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Handle your account and transactions effortlessly."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1690.75}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
